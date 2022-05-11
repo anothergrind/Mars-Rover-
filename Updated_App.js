e536e068e3824c3e2433b4c1b8d40a15bfb07ea2
RedGrind <97067525+RedGrind@users.noreply.github.com>
2022-05-11 06:43:54 -0500

    Updated App_js
    I changed the scope of the get methods so that it's in the app. Since it's in the app, I can set the state directly in the function

--- Updated_App.js
+++ Updated_App.js
@@ -1,20 +1,6 @@
-import './App.css';
+mport './App.css';
 import React, { useState } from 'react';
 
-const getCamera = (c) => {
-  const inputedCamera = c.target.value;
-  console.log(inputedCamera);
-}
-
-const getRover = (r) => {
-  const inputedRover = r.target.value;
-  console.log(inputedRover)
-}
-
-const getDate = (d) => {
-  const inputedDate = d.target.value;
-  console.log(inputedDate);
-}
 const constructURL = (camera, rover, date) => {
   const userURL = "https://api.nasa.gov/mars-photos/api/v1/rovers/" + rover + "/photos?earth_date=" + date + "&camera=" + camera + "&api_key=DEMO_KEY";
   console.log(userURL)
@@ -27,6 +13,23 @@ const App = () => {
   const [rover, setRover] = useState('');
   const [date, setDate] = useState('');
 
+  const getDate = (d) => {
+    const inputedDate = d.target.value;
+    setDate(d.target.value);
+    console.log(inputedDate);
+  }
+
+  const getRover = (r) => {
+    const inputedRover = r.target.value;
+    setRover(r.target.value);
+    console.log(inputedRover);
+  }
+
+  const getCamera = (c) => {
+    const inputedCamera = c.target.value;
+    setCamera(c.target.value);
+    console.log(inputedCamera);
+  }
   const gettingInfo = () => {
     fetch(constructURL(camera, rover, date))
     .then((response) => {setPhotos(response.json()['photos'])}) //sets values for photos 
@@ -39,9 +42,9 @@ const App = () => {
       <header className="App-header">
         <div id='container'>
           <div>
-            <input type="text" id="date-picker" name="date-picker" placeholder="Enter a date" onChange={(d) => setDate(d.target.value)}></input>
-            <input type="text" id="rover" name="rover" placeholder="Enter a rover" onChange={(r) => setRover(r.target.value)}></input>
-            <input type="text" id="camera" name="camera" placeholder="Enter a camera" onChange={(c) => setCamera(c.target.value)}></input>
+            <input type="text" id="date-picker" name="date-picker" placeholder="Enter a date" onChange={getDate}></input>
+            <input type="text" id="rover" name="rover" placeholder="Enter a rover" onChange={getRover}></input>
+            <input type="text" id="camera" name="camera" placeholder="Enter a camera" onChange={getCamera}></input>
           </div>
           <button onClick={gettingInfo}> Search </button>
         </div>
